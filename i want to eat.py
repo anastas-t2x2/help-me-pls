7b1d2f7699034ef2c27c65331628b521833cb50d
anastasis <anastasiya.eliseeva.2007@gmail.com>
2024-08-02 00:35:49 +0300

    my updates

--- i want to eat.py
+++ i want to eat.py
@@ -1,9 +1,9 @@
 import math
 name = input('Your name is: ')
 surname = input('Your surname is: ')
-hight = float(input('Your hight is: '))
+height = float(input('Your height is: '))
 print('Nice to meet you ;)')
 print('Fine')
 print('Your name is ' + name.capitalize())
-print('Your suename is ' + surname.capitalize())
-print('Your hight is almost ' + str(math.ceil(hight)))
+print('Your surname is ' + surname.capitalize())
+print('Your height is almost ' + str(math.ceil(height)))
