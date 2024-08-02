63bb39165a494cbb35bc03207d1bc717cc0753d1
anastasis <anastasiya.eliseeva.2007@gmail.com>
2024-08-03 00:28:39 +0300

    my updates

--- heh.py
+++ heh.py
@@ -1,20 +1,55 @@
-a = ['anastas', 'timon', 'chimin', 'bts']
+import time
+
+admin_password = 12345 #Для админов можно потом сделать как-нить отдельно создание пароля :) (через int(input()) само собой:) )
+user_names = ['anastas', 'timon', 'chimin', 'bts']
 name = None
+
+spisok = len(user_names)
 while name != '':   
     print('Hi!!! Enter your name.', "If you`re an administrator, write here 'admin'.", end='\n')
     name = input()
     if name == 'admin':
         print('Enter your password...')
-        pw = int(input())
-        while pw != 12345:
-            print('Wrong!')
-            break
+        password = int(input())
+        if password != admin_password:
+            print('Wrong, try again!')
+            password = int(input())
+            if password != admin_password:
+                print('Wrong, try again!')
+                password = int(input())
+                if password != admin_password:
+                    print('Wrong, try again!')
+                    print('Stop!!! You can try again only in')
+                    for i in range(5,0,-1):
+                        print(i)
+                        time.sleep(1)
+                    break
+                else:
+                    print('welcome. Here is your list.')
+                    print(user_names)
+                    break
+            else:
+                print('welcome. Here is your list.')
+                print(user_names)
+                break
         else:
             print('welcome. Here is your list.')
-            print(a)
+            print(user_names)
             break
     else:
-        print('Hi')
-        a.append(name)
-        break
-        
+        count = 0
+        for i in range(0,spisok):
+            if name == user_names[count]:
+                print('Would you like to delete yourself? Enter "Yes" or "No".')
+                say = input()
+                if say == 'Yes':
+                    user_names.remove(name)
+                    print('Done.')
+                    break
+                else:
+                    print('OK')
+                    break
+            else:
+                count += 1
+print(':)')
+                    
